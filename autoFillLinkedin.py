126fe312fbceb9d66b295e4ae0abcedc06c2def1
CowboyAragorn <jacobman0313@gmail.com>
2023-04-12 17:20:06 -0400

    update xml paths after linkedin update

--- autoFillLinkedin.py
+++ autoFillLinkedin.py
@@ -9,31 +9,32 @@ import openpyxl
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill, Font
 from datetime import date
+import traceback
 
 
-print('Paste linkedin URL while clicked on the job you just applied to')
+print('Right click the name of a job on linkedin\'s job page, copy the link, and paste it here:')
 url = input()
 
 #TODO make headless an option
-# # #runs selenium browser invisibly so you can just operate from CLI
-# chrome_options = Options()
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument("--window-size=1920,1080")
-# chrome_options.add_argument('--ignore-certificate-errors')
-# chrome_options.add_argument('--allow-running-insecure-content')
-# browser = webdriver.Chrome(options=chrome_options)
+# runs selenium browser invisibly so you can just operate from CLI
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--allow-running-insecure-content')
+browser = webdriver.Chrome(options=chrome_options)
 
 print('Loading Linkedin...')
-browser = webdriver.Chrome()
+# browser = webdriver.Chrome()
 browser.get(url)
 browser.maximize_window()
-time.sleep(2)
+time.sleep(3)
 
 print('Navigating to sign in page...')
 #first sign in click on linkedin
 signInBtn1 = browser.find_element(By.XPATH, '/html/body/div[3]/a[1]')
 signInBtn1.click()
-time.sleep(2)
+time.sleep(3)
 
 print('Inputting credentials...')
 #second sign in click on linkedin
@@ -50,7 +51,8 @@ signInBtn2 = browser.find_element(By.XPATH, '/html/body/div/main/div[2]/div[1]/f
 usernameInput.send_keys(username)
 passwordInput.send_keys(password)
 signInBtn2.click()
-time.sleep(20)
+#time.sleep(20)
+time.sleep(5)
 
 print('Login Successful!')
 #If this was a larger project, make a dict for each of the values keeping info like cell, innerhtml so that you don't have to manually update when making changes
@@ -100,17 +102,18 @@ if int(rowToWrite)%2 == 0:
 
 
 #try to find the element and write it to the sheet. If anything fails, easy exit with an except and print an error
-#TODO: add date, easyapply, etc
+#TODO: Perhaps switch link to jobs specifc link
+
 #NAME
 try:
-    name = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[4]/div/div/main/div/section[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/a/h2')
+    name = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div[1]/div[1]/div/div[1]/div/div/div[1]/h1')
     #print(name.text)
     ws['A' + rowToWrite] = str(name.text)
 except: 
     print('name failed')
 #COMPANY
 try:
-    company = browser.find_element(By.XPATH,'/html/body/div[5]/div[3]/div[4]/div/div/main/div/section[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[1]/span[1]/span[1]/a' )
+    company = browser.find_element(By.XPATH,'/html/body/div[5]/div[3]/div/div[1]/div[1]/div/div[1]/div/div/div[1]/div[1]/span[1]/span[1]/a' )
     #print(company.text)
     ws['B' + rowToWrite] = str(company.text)
 except: 
@@ -124,32 +127,34 @@ except:
     print('Date failed')
 #LOCATION
 try:
-    location = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[4]/div/div/main/div/section[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[1]/span[1]/span[2]')
+    location = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div[1]/div[1]/div/div[1]/div/div/div[1]/div[1]/span[1]/span[2]')
     #print(location.text)
     ws['D' + rowToWrite] = str(location.text)
 except: 
     print('location failed')
 #LINK
 try:
-    postingLink = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[4]/div/div/main/div/section[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/a')
-    postingLink = postingLink.get_attribute('href')
-    #print(postingLink)
-    ws['E' + rowToWrite] = str(postingLink)
+    # postingLink = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[4]/div/div/main/div/section[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/a')
+    # postingLink = postingLink.get_attribute('href')
+    # ws['E' + rowToWrite] = str(postingLink)
+    #just take url from original user input
+    ws['E' + rowToWrite] = str(url)
 except: 
     print('link failed')
 #SALARY
 try:
-    salary = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[4]/div/div/main/div/section[2]/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[1]/span/a')
+    salary = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div[1]/div[1]/div/div[6]/div[1]/div/div[2]/p')
     #print(postingLink)
     ws['F' + rowToWrite] = str(salary.text)
 except:
     print('No Salary Available')
 #WORKPLACE TYPE
 try:
-    remote = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[4]/div/div/main/div/section[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[1]/span[1]/span[3]')
+    remote = browser.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div[1]/div[1]/div/div[1]/div[1]/div/div[1]/div[1]/span[1]/span[3]')
     ws['G' + rowToWrite] = str(remote.text)
 except: 
     print('No Remote Info Available')
+
 editBook.save(cwd+'/Job_Search_Log.xlsx')
 #TODO: tell what you are writing to workbook, ie writing [name,company]
 print('Workbook saved!')
